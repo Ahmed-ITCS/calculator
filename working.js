3e0b97c6adc63c85bf38ae5933f5ae87e0129470
Ahmed-ITCS <59608481+Ahmed-ITCS@users.noreply.github.com>
2021-10-09 16:25:15 +0500

    Add files via upload

---
+++ working.js
@@ -0,0 +1,101 @@
+
+let screen = document.getElementById('screen');
+let buttons = document.querySelectorAll('button');
+let screenvalue = "";
+for(let item of buttons){
+	item.addEventListener('click' , (e)=>{
+		let buttonText = e.target.innerText;
+		if (buttonText=='*') {
+			buttonText = '*';
+			screenvalue += buttonText
+			screen.value += buttonText;
+		}
+		else if (buttonText=='C') {
+			screenvalue="";
+			screen.value = "";
+		}
+		else if (buttonText=='/') {
+			screenvalue += buttonText
+			screen.value += buttonText;
+		}
+		else if (buttonText=='+') {
+			screenvalue += buttonText
+			screen.value += buttonText;
+		}
+		else if (buttonText=='-') {
+			screenvalue += buttonText
+			screen.value += buttonText;
+		}
+		else if (buttonText=='=')
+		{
+			screen.value = eval(screenvalue);
+			console.log(screenvalue)
+			console.log("bingooo")
+		}
+		else if (buttonText=='1')
+		{
+			screenvalue += buttonText
+			screen.value += buttonText;
+			//screen.value = eval(screenvalue);
+		}
+		else if (buttonText=='2')
+		{
+			screenvalue += buttonText
+			screen.value += buttonText;
+			//screen.value = eval(screenvalue);
+		}
+
+		else if (buttonText=='3')
+		{
+			screenvalue += buttonText
+			screen.value += buttonText;
+			//screen.value = eval(screenvalue);
+		}
+		else if (buttonText=='4')
+		{
+			screenvalue += buttonText
+			screen.value += buttonText;
+			//screen.value = eval(screenvalue);
+		}
+		else if (buttonText=='5')
+		{
+			screenvalue += buttonText
+			screen.value += buttonText;
+			//screen.value = eval(screenvalue);
+		}
+		else if (buttonText=='6')
+		{
+			screenvalue += buttonText
+			screen.value += buttonText;
+			//screen.value = eval(screenvalue);
+		}
+		else if (buttonText=='7')
+		{
+			screenvalue += buttonText
+			screen.value += buttonText;
+			//screen.value = eval(screenvalue);
+		}
+		else if (buttonText=='8')
+		{
+			screenvalue += buttonText
+			screen.value += buttonText;
+			//screen.value = eval(screenvalue);
+		}
+		else if (buttonText=='9')
+		{
+			screenvalue += buttonText
+			screen.value += buttonText;
+			//screen.value = eval(screenvalue);
+		}
+		else if (buttonText=='0')
+		{
+			screenvalue += buttonText
+			screen.value += buttonText;
+			//screen.value = eval(screenvalue);
+		}
+
+	//screenvalue += buttonText	
+
+	})
+
+}
